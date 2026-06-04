18fab2bb926b7cde5b969f87690a6bae092868fd
van1sh <agostino.panico@gmail.com>
2026-06-04 19:21:10 +0000

    Modernize build: target .NET 8 LTS and update dependencies
    - Upgrade target framework from net6.0 (EOL) to net8.0 (LTS)
    - Update NuGet packages: LibGit2Sharp 0.31.0, McMaster.CommandLineUtils 4.1.1,
      Microsoft.Build 17.11.4, System.Configuration.ConfigurationManager 8.0.1
    - Migrate unmaintained YamlDotNet.NetCore -> YamlDotNet 18.0.0
    - Adapt GitHelpers to LibGit2Sharp CloneOptions.FetchOptions API
    - Replace obsolete WebClient download path with HttpClient
    - Add nuget.config so bundled DonutCore resolves from ExternalResources
    - Bump app version to 2.1.0; add project metadata
    - Stop tracking user-specific OffensivePipeline.csproj.user

--- OffensivePipeline/Helpers.cs
+++ OffensivePipeline/Helpers.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO.Compression;
 using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +12,8 @@ namespace OffensivePipeline
 {
     internal class Helpers
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         public static string GetRandomString()
         {
             string path = Path.GetRandomFileName();
@@ -24,11 +24,15 @@ namespace OffensivePipeline
         public static bool DownloadResources(string url, string outputName, string outputPath)
         {
             bool status = true;
-            WebClient client = new WebClient();
             try
             {
                 string f = Path.Combine(Directory.GetCurrentDirectory(), outputPath, outputName);
-                client.DownloadFile(url, f);
+                using (var response = httpClient.GetAsync(url).GetAwaiter().GetResult())
+                {
+                    response.EnsureSuccessStatusCode();
+                    using var fs = new FileStream(f, FileMode.Create, FileAccess.Write, FileShare.None);
+                    response.Content.CopyToAsync(fs).GetAwaiter().GetResult();
+                }
                 if (!File.Exists(f))
                 {
                     LogHelpers.PrintError($"DownloadResources: File not found <{f}>");
